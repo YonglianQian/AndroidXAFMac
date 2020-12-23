5597c6b419e3ac49bf3966263d4ad5256d1c9f2e
Abraham <qianyonglian@outlook.com>
2020-12-23 17:12:28 +0800

    AndroidXAFMac first commit

---
+++ AndroidXAFMac/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AndroidXAFMac
+{
+    public partial class MainPage : ContentPage
+    {
+        int counter = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+
+            counter++;
+            MyButton.Text = $"You clicked {counter} times";
+        }
+    }
+}
